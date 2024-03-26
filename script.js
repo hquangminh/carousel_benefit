5c8b3d674341761004dca53a4fe334fbfdafe8a8
charleshuynh <charleshuynh.connect@gmail.com>
2024-03-26 11:24:49 +0700

    check commit product

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-let index = 0
-
-function moveCarousel(step) {
+document.addEventListener('DOMContentLoaded', () => {
   const features = document.querySelectorAll('.feature')
-  const totalFeatures = features.length
-  const featuresToShow = 2
-  index = (index + step + totalFeatures / featuresToShow) % (totalFeatures / featuresToShow)
-  const offset = index * -100
-  const innerCarousel = document.querySelector('.carousel-inner')
-  innerCarousel.style.transform = `translateX(${offset}%)`
-}
+  const dotsContainer = document.querySelector('.carousel-dots')
+  let currentSlide = 0
+
+  function createDots() {
+    const numSlides = Math.ceil(features.length / 2)
+    for (let i = 0; i < numSlides; i++) {
+      const dot = document.createElement('div')
+      console.log('dot', dot)
+      dot.classList.add('dot')
+      dot.addEventListener('click', () => goToSlide(i))
+      dotsContainer.appendChild(dot)
+    }
+    updateDots()
+  }
+
+  function updateDots() {
+    const dots = document.querySelectorAll('.dot')
+    dots.forEach((dot, index) => {
+      dot.classList.remove('active')
+      if (index === currentSlide) {
+        dot.classList.add('active')
+      }
+    })
+  }
+
+  function goToSlide(slideIndex) {
+    const offset = slideIndex * -100
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`
+    currentSlide = slideIndex
+    updateDots()
+  }
+
+  createDots()
+  goToSlide(0)
+})
